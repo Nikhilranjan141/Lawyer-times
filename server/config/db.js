9233f9257c36a151f27ab7a3f75f16c85c244921
Nikhilranjan141 <nikhilranjanlkr@gmail.com>
2026-05-29 23:45:10 +0530

    update db.js1
    Enhanced error logging and ensured MONGO_URI presence.

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,59 @@
-const mongoose=require("mongoose");
+// const mongoose=require("mongoose");
 
-const connectDB=async()=>{
+// const connectDB=async()=>{
 
-try{
+// try{
 
-await mongoose.connect(
-process.env.MONGO_URI
-)
+// await mongoose.connect(
+// process.env.MONGO_URI
+// )
 
-console.log("MongoDB Connected")
+// console.log("MongoDB Connected")
 
-}
+// }
 
-catch(error){
+// catch(error){
 
-console.log(
-"DB Error:",
-error.message
-)
+// console.log(
+// "DB Error:",
+// error.message
+// )
 
-process.exit(1)
+// process.exit(1)
 
-}
+// }
 
-}
+// }
 
-module.exports=connectDB
+// module.exports=connectDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log("MONGO URI EXISTS:", !!process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("FULL DB ERROR:", error);
+    console.log("DB Error:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
